9931098117dd712c5abb67afa44e65083d790eda
Pedro de Oliveira Machado <pedromachado.2003@alunos.utfpr.edu.br>
2024-08-30 02:16:18 -0300

    Home flex fixed

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -5,11 +5,12 @@ const Header = () => {
         <img className="h-24" src="/logo.png" alt="Logo" />
         <ul className="flex items-center justify-center gap-20">
           <li className="transition-all duration-300 hover:text-orange-600 hover:text-lg active:text-orange-200 lg:block sm:hidden">
-            <a href="/">
-              Página inicial
-            </a>
+            <a href="/">Página inicial</a>
           </li>
-          <li className="flex items-center py-2 px-4 text-white rounded-full cursor-pointer bg-purple-600 transition-all duration-300 hover:bg-orange-600 hover:px-8 active:bg-orange-200 active:text-slate-500">
+          <li
+            className="flex items-center py-2 px-4 text-white rounded-full cursor-pointer bg-purple-600 transition-all duration-300
+          hover:bg-orange-600 hover:px-8 active:bg-orange-200 active:text-slate-500"
+          >
             <a href="/login" className="block">
               Entrar
             </a>
