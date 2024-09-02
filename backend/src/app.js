8151b8b84f293a3558e4d2e63f033bd0c5afe33d
Pedro de Oliveira Machado <pedromachado.2003@alunos.utfpr.edu.br>
2024-09-02 19:44:37 -0300

    automated attacks prevention working

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const router = require('./router');
-
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
-app.use(cors());
 
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 50,
+  message: "Too many requests from this IP, please try again later."
+});
+
+app.use(cors());
 app.use(express.json());
+app.use(limiter);
 app.use(router);
 
 module.exports = app;
