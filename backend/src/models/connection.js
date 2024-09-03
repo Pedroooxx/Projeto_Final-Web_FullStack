d78420dddc8feb716e8019fe1ff09f7c7a5af0e5
Pedro de Oliveira Machado <pedromachado.2003@alunos.utfpr.edu.br>
2024-09-03 19:39:18 -0300

    connection pool config

--- backend/src/models/connection.js
+++ backend/src/models/connection.js
@@ -2,25 +2,29 @@ const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const connection = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DB,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
+  waitForConnections: true,
+  connectionLimit: 100,
+  queueLimit: 10,
+  idleTimeout: 60000
 });
 
 const { createClient } = require("@redis/client");
 
 const redisClient = createClient({
-    url: 'redis://localhost:6379',
-  });
-  
-  redisClient.on('error', (err) => {
-    console.error('Redis error:', err);
-  });
-  
-  redisClient.connect();
+    url: 'redis://localhost:6379',
+  });
+  
+redisClient.on('error', (err) => {
+    console.error('Redis error:', err);
+  });
+  
+redisClient.connect();
 
 module.exports = {
-    connection,
-    redisClient
+    connection,
+    redisClient
 };
